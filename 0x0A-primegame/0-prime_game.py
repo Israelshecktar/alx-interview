b6814d78a8fe6c1e903a01bcc97aac31cc4bb950
Israelshecktar <iamshecktar1996@gmail.com>
2024-07-04 07:40:20 +0000

    fixed lint

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,7 +3,7 @@
 
 
 def isWinner(x, nums):
-    """Check winner of a game"""
+    """ Check winner of a game """
     if x < 1 or not nums:
         return None
 
@@ -13,13 +13,13 @@ def isWinner(x, nums):
     n = max(nums)
     primes = [True] * (n + 1)
     primes[0] = primes[1] = False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if primes[i]:
             for j in range(i * i, n + 1, i):
                 primes[j] = False
 
     for n in nums:
-        count = sum(primes[2 : n + 1])
+        count = sum(primes[2:n + 1])
         if count % 2 == 0:
             ben_success += 1
         else:
